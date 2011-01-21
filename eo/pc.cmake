c3c4aad269a81cad530d13bdc0aa2ce321fb8ac1
Caner Candan <caner.candan@thalesgroup.com>
2011-01-21 11:24:23 +0100

    * eo.pc

--- eo/pc.cmake
+++ eo/pc.cmake
@@ -3,4 +3,4 @@ Name: @PACKAGE_NAME@
 Description: @PACKAGE_NAME@
 Version: @PROJECT_VERSION@
 Libs: -L@PCPREFIX@/lib @PCFLAGS@
-Cflags: -I@PCPREFIX@/include/@PCINCLUDEDIR@
+#Cflags: -I@PCPREFIX@/include/@PCINCLUDEDIR@
